e9f87631bc3933ead066a4b6d12c49f2e8454e7c
John P. Willis <jpw@coherent-logic.com>
2014-07-13 15:48:03 -0600

    Add MUMPS routine skeleton

---
+++ src/KBBMCIDT.m
@@ -0,0 +1,63 @@
+KBBMCIDT ;CLD/JPW - GT.M CALLIN DATA ACCESS API;7/13/2014
+;;0.01;CIDT;**PATCHES**;7/13/2014;Build 0
+;; Copyright (C) 2014 Coherent Logic Development LLC
+;;  All Rights Reserved
+;;
+;; Author:  John P. Willis <jpw@coherent-logic.com>
+;; Written: 7/12/2014
+;;
+;;  This file is part of gtmci-data-api.
+;;
+;;  gtmci-data-api is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Affero General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  gtmci-data-api is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+;;
+;;  You should have received a copy of the GNU Affero General Public License
+;;  along with gtmci-data-api.  If not, see <http://www.gnu.org/licenses/>. 
+;;
+ QUIT ""
+ ;
+SET(GLVN,VALUE)
+ QUIT 1
+ ;
+ ;
+GET(GLVN)
+ QUIT 1
+ ;
+ ;
+KILL(GLVN)
+ QUIT 1
+ ;
+ ;
+DATA(GLVN)
+ QUIT 1
+ ;
+ ;
+ORDER(GLVN)
+ QUIT 1
+ ;
+ ;
+QUERY(GLVN)
+ QUIT 1
+ ;
+ ;
+LOCK(GLVN)
+ QUIT 1
+ ;
+ ;
+UNLOCK(GLVN)
+ QUIT 1
+ ;
+ ;
+VERSION()
+ QUIT 1
+ ;
+ ;
+FUNCTION(FN)
+ QUIT 1
